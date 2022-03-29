a85b13f05f0be641ed1d5015f7b6e7f8b1031a15
Akash <akash951920@gmail.com>
2022-03-29 15:50:56 +0530

    modal and backdrop addes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import Modal from "./components/Modal";
 import Todo from "./components/Todo"
+import Backdrop from './components/Backdrop'
+
 function App() {
   return (
     <div>
@@ -6,6 +9,8 @@ function App() {
       <Todo text="Learn React"/>
       <Todo text="Master React"/>
       <Todo text="Explore the full React"/>
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
